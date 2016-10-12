79a0a1bc07dd106976d7484fc92dca6d65c93dc0
Miles Smith <miles71397@gmail.com>
2016-10-12 11:15:28 -0700

    change for heroku

--- public/client/app.js
+++ public/client/app.js
@@ -16,6 +16,8 @@ window.Shortly = Backbone.View.extend({
     Backbone.history.start({ pushState: true });
   },
 
+  
+
   render: function() {
     this.$el.html( this.template() );
     return this;
